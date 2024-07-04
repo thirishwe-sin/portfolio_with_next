358307292aeff16abcc11462b47df0a2aa45e98b
thirishwe-sin <thirishwesinsin@gmail.com>
2024-07-04 17:24:21 +0630

    projects ongoing

---
+++ components/RecentProject.tsx
@@ -0,0 +1,31 @@
+import { projects } from '@/data';
+import React from 'react';
+import { PinContainer } from './ui/3d-pin';
+
+const RecentProject = () => {
+    return (
+        <div className='py-20'>
+            <h1 className='heading text-white'>
+                A small selection of {' '}
+                <span className='text-purple'>recent projects</span>
+            </h1>
+            <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
+                {projects.map(({id, title, des,img,iconLists,link}) => (
+                    <div key={id} className='text-white lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
+                        <PinContainer title={title} href={link} >
+                            <div>
+                                <div>
+                                    <img src="/bg.png" alt="bg-img" />
+                                </div>
+                                <img src={img} alt={title} className='z-10 ' />
+                            </div>
+                        </PinContainer>
+                    </div>
+                ))}
+
+            </div>
+        </div>
+    );
+}
+
+export default RecentProject;
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,13 @@
+"use client";
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
-import  GlobeDemo  from "./GridGlobe";
+import GlobeDemo from "./GridGlobe";
+import { useState } from "react";
+import animationData from "@/data/confetti.json";
+import Lottie from "react-lottie";
+import MagicButton from "./MagicButton";
+import { IoCopyOutline } from "react-icons/io5";
+import '../css/ScrollStyle.css'
 
 export const BentoGrid = ({
   className,
@@ -42,10 +49,20 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText("thirishwesinsin@gmail.com");
+
+    setCopied(true);
+  };
+  const items1 = ["React.js", "Next.js", "Typescript", "PHP", "Laravel", "ReactNative", "TailwindCSS", "Bootstrap"];
+  const items2 = ["AWS", "MongoDB", "MySQL", "Git", "GitHub", "GitLab"];
+
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
+        "row-span-1 relative overflow-hidden rounded-3xl  group/bento hover:shadow-xl transition duration-200 shadow-input border border-white/[0.1] dark:shadow-none justify-between flex flex-col space-y-4",
         className
       )}
       style={{
@@ -54,7 +71,7 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(56,56,68,1) 26%, rgba(93,108,111,1) 100%",
       }}
     >
-      <div className={`${id === 6} && 'flex justify-center h-full`}>
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
             <img
@@ -96,10 +113,86 @@ export const BentoGridItem = ({
           >
             {title}
           </div>
+          {id === 2 && <GlobeDemo />}
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              <div className="scroll-container">
+                <div className="scroll-wrapper">
+                  <div className="scroll-content">
+                    {items1.map((item, index) => (
+                      <span
+                        key={index}
+                        className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E] text-white"
+                      >
+                        {item}
+                      </span>
+                    ))}
+                  </div>
+                  <div className="scroll-content">
+                    {items1.map((item, index) => (
+                      <span
+                        key={index + items1.length}
+                        className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E] text-white"
+                      >
+                        {item}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="scroll-container">
+                <div className="scroll-wrapper">
+                  <div className="scroll-content">
+                    {items2.map((item, index) => (
+                      <span
+                        key={index}
+                        className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E] text-white"
+                      >
+                        {item}
+                      </span>
+                    ))}
+                  </div>
+                  <div className="scroll-content">
+                    {items2.map((item, index) => (
+                      <span
+                        key={index + items2.length}
+                        className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E] text-white"
+                      >
+                        {item}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+          {id === 6 && (
+            <div className="mt-5 relative">
+              <div className={`absolute -bottom-5 right-0`}>
+                <Lottie
+                  options={{
+                    loop: copied,
+                    autoplay: copied,
+                    animationData,
+                    rendererSettings: {
+                      preserveAspectRatio: "xMidYMid slice",
+                    },
+                  }}
+                />
+              </div>
+
+              <MagicButton
+                title={copied ? "Email copied" : "Copy my email"}
+                icon={<IoCopyOutline />}
+                position="left"
+                otherClasses="!bg-[#161a31]"
+                handleClick={handleCopy}
+              />
+            </div>
+          )}
         </div>
-        {id === 2 && <GlobeDemo />}
+        {/* <div className="group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"></div> */}
       </div>
-      {/* <div className="group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"></div> */}
     </div>
   );
 };
